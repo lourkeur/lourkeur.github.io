a0fecebba1feb103d7f50798369c468b75919263
Louis Bettens <louis@bettens.info>
2022-06-19 18:29:19 +0200

    nix: build website

--- nix/package.nix
+++ nix/package.nix
@@ -1,8 +1,21 @@
-{stdenv}:
-stdenv.mkDerivation {
+{
+  stdenvNoCC,
+  hugo,
+}:
+stdenvNoCC.mkDerivation {
   name = "blog";
   src = builtins.path {
-    path = ../.;
+    path = ../blog;
     name = "source";
   };
+
+  buildInputs = [hugo];
+
+  buildPhase = ''
+    hugo
+  '';
+
+  installPhase = ''
+    cp -R public $out
+  '';
 }
